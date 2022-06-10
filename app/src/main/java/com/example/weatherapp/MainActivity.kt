65941d87c9c991c5b6ae0e49d2ee34dfed845ced
ngov94 <neena.govindhan@yahoo.ca>
2022-06-10 12:18:13 -0400

    Fragments implemented and CurrentLocation page working

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -12,11 +12,9 @@ import com.example.weatherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-//    lateinit var vm : WeatherViewModel
 
-    //---------------
     private lateinit var binding: ActivityMainBinding
-//--------------
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,67 +34,9 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-//--------------------------
+
     }
 }
 
 
 
-
-//        setContentView(R.layout.activity_main)
-
-//        vm.currentLocation.observe(this){ it ->
-////            println(it)
-//            var latitude = it.location.lat.toString()
-//            var longitude = it.location.lng.toString()
-//            vm.getCurrentCity("$latitude+$longitude", openCageDataKey)
-//            vm.getCurrentWeather(latitude, longitude, weatherApiKey, units)
-//        }
-//
-////<----Codes for Google autocomplete Fragment. More here: https://developers.google.cn/maps/documentation/places/android-sdk/autocomplete--->
-//        Places.initialize(applicationContext, googleApi)
-//        placesClient = Places.createClient(this)
-//        // Initialize the AutocompleteSupportFragment.
-//        val autocompleteFragment =
-//            supportFragmentManager.findFragmentById(R.id.autocomplete_fragment)
-//                    as AutocompleteSupportFragment
-//
-//        autocompleteFragment.setTypeFilter(TypeFilter.CITIES)
-//        // Specify the types of place data to return.
-//        autocompleteFragment.setPlaceFields(listOf(Place.Field.LAT_LNG, Place.Field.ADDRESS))
-//
-//        // Set up a PlaceSelectionListener to handle the response.
-//        autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
-//            override fun onPlaceSelected(place: Place) {
-//                // TODO: Get info about the selected place.
-//                println("Place: ${place.address}, ${place.latLng}")
-//                tv_city_name.text = place.address
-//                var latitude = place.latLng?.latitude.toString()
-//                var longitude = place.latLng?.longitude.toString()
-//                vm.getCurrentWeather(latitude, longitude, weatherApiKey, units)
-//            }
-//
-//            override fun onError(status: Status) {
-//                println("An error occurred: $status")
-//            }
-//        })
-////        <---End of Codes for Google autocomplete Fragment --->
-//
-//
-//
-//        vm.currentWeather.observe(this) {
-//            val gson = GsonBuilder().setPrettyPrinting().create()
-//            val pJson = gson.toJson(it)
-
-//        vm.currentCity.observe(this){
-//            println(it)
-//            var city = it.results[0].components.city
-//            if(city == null) city = it.results[0].components.county
-//            var state = it.results[0].components.state
-//            var country = it.results[0].components.country
-//            var placeName = "$city, $state, $country"
-//            tv_city_name.text = placeName
-//        }
-
-
-//--------------------------
