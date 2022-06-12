f79cc0abbf5ad3849dfbfa79769de7710c32f4a4
ngov94 <neena.govindhan@yahoo.ca>
2022-06-12 18:13:19 -0400

    Disabled fragment recreation

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -4,16 +4,27 @@ package com.example.weatherapp
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.weatherapp.databinding.ActivityMainBinding
+import com.example.weatherapp.ui.currentlocation.CurrentLocationFragment
+import com.example.weatherapp.ui.locations.LocationsFragment
+import com.example.weatherapp.ui.weekly.WeeklyFragment
 
 class MainActivity : AppCompatActivity() {
 
 
     private lateinit var binding: ActivityMainBinding
+    val fragmentCurrentLocation = CurrentLocationFragment()
+    val fragmentWeekly = WeeklyFragment()
+    val fragmentLocation = LocationsFragment()
+    val fm: FragmentManager = supportFragmentManager
+    var active: Fragment = fragmentCurrentLocation
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +32,10 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        fm.beginTransaction().add(R.id.nav_host_fragment_activity_main, fragmentLocation, "3").hide(fragmentLocation).commit()
+        fm.beginTransaction().add(R.id.nav_host_fragment_activity_main, fragmentWeekly, "2").hide(fragmentWeekly).commit()
+        fm.beginTransaction().add(R.id.nav_host_fragment_activity_main, fragmentCurrentLocation, "1").commit()
 
 
         val navView: BottomNavigationView = binding.bottomNav
@@ -30,11 +45,32 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.currentlocationFragment2, R.id.weeklyFragment2,  R.id.locationsFragment2, R.id.mapFragment2
+                R.id.currentlocationFragment2, R.id.weeklyFragment2,  R.id.locationsFragment2
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        navView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.currentlocationFragment2 ->{
+                    fm.beginTransaction().hide(active).show(fragmentCurrentLocation).commit()
+                    active = fragmentCurrentLocation
+                    true
+                }
+                R.id.weeklyFragment2 ->{
+                    fm.beginTransaction().hide(active).show(fragmentWeekly).commit()
+                    active = fragmentWeekly
+                    true
+                }
+                R.id.locationsFragment2 ->{
+                    fm.beginTransaction().hide(active).show(fragmentLocation).commit()
+                    active = fragmentLocation
+                    true
+                }
+                else -> false
+            }
+        }
 
     }
 }
